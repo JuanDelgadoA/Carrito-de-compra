4d3a17f2dd0f882074305a798caa6e2527a34cd5
bryan <bryan.amaro.vaz@gmail.com>
2022-09-01 22:08:27 -0500

    intento de baner x . x

--- html/index.php
+++ html/index.php
@@ -40,10 +40,10 @@
 <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
   <div class="carousel-inner ">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="../multimedia/baner-index1.jfif" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item ">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="..." class="d-block w-100" >
     </div>
     <div class="carousel-item ">
       <img src="..." class="d-block w-100" alt="...">
